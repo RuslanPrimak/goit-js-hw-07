179149e9105b5b5d7253e932af59451f595f0638
RuslanPrymak <ruslan.prymak@globallogic.com>
2021-09-02 23:34:20 +0300

    task 4

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,31 +1,25 @@
-"use strict";
+'use strict';
 
 const images = [
   {
-    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
   },
   {
-    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
   },
 ];
 
 const markup = images.reduce(
   (string, image) =>
     string + `<li><img src=${image.url} alt=${image.alt}></li>`,
-  ""
+  ''
 );
 
-const imagesElement = document.querySelector("#gallery");
-imagesElement.insertAdjacentHTML("beforeend", markup);
-imagesElement.style.display = "flex";
-// imagesElement.style.justifyContent = "center";
-// imagesElement.style.justifyContent = "space-around";
-// imagesElement.style.height = "5em";
-// imagesElement.style.alignItems = "center";
-imagesElement.style.alignItems = "baseline";
+const imagesElement = document.querySelector('#gallery');
+imagesElement.insertAdjacentHTML('beforeend', markup);
--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,1 +1,37 @@
-"use strict";
+'use strict';
+
+class SpanCounter {
+  constructor(span) {
+    this._span = span;
+    this._counter = Number(span.textContent);
+  }
+
+  updateSpanValue() {
+    this._span.textContent = this._counter;
+  }
+
+  increment() {
+    this._counter += 1;
+    this.updateSpanValue();
+  }
+
+  decrement() {
+    this._counter -= 1;
+    this.updateSpanValue();
+  }
+}
+
+const spanElement = document.querySelector('#counter #value');
+
+const counter = new SpanCounter(spanElement);
+
+const incrementBtn = document.querySelector(
+  '#counter button[data-action="increment"]'
+);
+
+const decrementBtn = document.querySelector(
+  '#counter button[data-action="decrement"]'
+);
+
+incrementBtn.addEventListener('click', counter.increment.bind(counter));
+decrementBtn.addEventListener('click', counter.decrement.bind(counter));
